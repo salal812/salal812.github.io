6ba9e51ef2c348b84f29368b7aeeb2727606eb83
salal812 <salal812@newschool.edu>
2021-03-12 08:30:47 -0500

    updated files

---
+++ JavaIntro/main.js
@@ -0,0 +1,76 @@
+console.log("hi");
+
+var myName = ("Lourdes");
+
+var myLastName = ("Salazar");
+
+var myFullName = (myName+myLastName);
+
+console.log(myName+myLastName);
+
+var areYouOk = true;
+
+var areYouGood = false;
+
+var integers = 333;
+console.log(integers);
+
+var oneHundred = "50+50"
+console.log("oneHundred");
+
+var mary = 'Supercalifragilisticexpialidocious';
+console.log(mary.length);
+
+var myArray = ['l', 'o', 'u', 'r', 'd', 'e', 's'];
+console.log(myArray);
+
+console.log(myName.length == myLastName.length);
+
+myArray.forEach(function(item) {
+	console.log(item);
+});
+
+var favRestaurants = [
+	"Tatsu Ramen",
+	"Tompkins square bagels",
+	"Dig Inn"
+];
+console.log(favRestaurants);
+
+
+var inTheMiddleOfPandemic = [
+	"Cant go to concerts",
+	"Cant go to the movies",
+	"Cant Travel"
+];
+console.log(inTheMiddleOfPandemic);
+
+var favPlacesNyc = [
+	"Washington Square Park",
+	"East Village",
+	"Museums"
+];
+console.log(favPlacesNyc);
+
+var favMovies = [
+	"The Shinning",
+	"Trainspotting",
+	"Mujeres al borde de un ataque de nervios"
+];
+console.log(favMovies);
+
+var lists = [favRestaurants, inTheMiddleOfPandemic, favPlacesNyc, favMovies];
+console.log(lists);
+
+
+
+var x = new Boolean(false); 
+
+var y = new Boolean(true);
+
+
+
+var myAnimalsArray = ['dog', 'cat', 'dolphin', 'fish', 'tiger']
+console.log(myAnimalsArray);
+
+
