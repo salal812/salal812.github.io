552e4f76afd42b3a4d0c02defba9217185f28cd2
salal812 <salal812@newschool.edu>
2021-05-04 08:56:49 -0400

    sounds

---
+++ ecosystem/main.js
@@ -0,0 +1,51 @@
+var one = new Audio();
+one.src = "soundList/cats.mp3";
+function PlayCats() {
+    one.play();
+}
+function PauseCats() {
+    one.pause();
+}
+var two = new Audio();
+two.src = "soundList/birds.wav";
+function PlayBirds() {
+    two.play();
+}
+function PauseBirds() {
+    two.pause();
+}
+var three = new Audio();
+three.src = "soundList/Cricket.wav";
+function PlayCricket() {
+    three.play();
+}
+function PauseCricket() {
+    three.pause();
+}
+
+var four= new Audio();
+four.src = "soundList/dog.wav";
+function PlayDog() {
+    four.play();
+}
+function PauseDog() {
+    four.pause();
+}
+
+var five = new Audio();
+five.src = "soundList/LevantSparrowhawk.wav";
+function PlayLevant() {
+    five.play();
+}
+function PauseLevant() {
+    five.pause();
+}
+
+var six = new Audio();
+six.src = "soundList/rainforest.mp3";
+function PlayRainforest() {
+    three.play();
+}
+function PauseRainforest() {
+    three.pause();
+}
