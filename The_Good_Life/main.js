84de7260e9b4f17c3a4853ea9373399c0add3d0b
salal812 <salal812@newschool.edu>
2022-04-27 15:00:17 -0400

    push

--- The_Good_Life/main.js
+++ The_Good_Life/main.js
@@ -1,26 +1,26 @@
 window.onload = init;
 	
 function init() {
-	myWallsIndex = 0;
+	myVidsIndex = 0;
 	let logo = document.getElementById('logo');
 	console.log(logo)
 	logo.addEventListener("click", nextWall, 1);
 }
 
-let myWallsArray = ["wall1", "wall2", "wall3", "wall4"];
-let myWallsIndex = 0;
+let myVidsArray = ["wall1", "wall2", "wall3", "wall4"];
+let myVidsIndex = 0;
 
 function nextWall(direction) {
 	console.log("clicked");
 	//increment / decrement array
-	myWallsIndex = myWallsIndex + 1;	
-	if (myWallsIndex == myWallsArray.length){
-		myWallsIndex = 0
+	myVidsIndex = myVidsIndex + 1;	
+	if (myVidsIndex == myVidsArray.length){
+		myVidsIndex = 0
 	}
 	// update 
 	element = document.getElementById('wall');
 	console.log("Your element is: ");
 	console.log(element);
-	console.log(myWallsIndex, myWallsArray[myWallsIndex]);
-	element.src = "images/" + myWallsArray[myWallsIndex] + ".png";
+	console.log(myVidsIndex, myVidsArray[myVidsIndex]);
+	element.src = "images/" + myVidsArray[myVidsIndex] + ".png";
 }
--- lou.js
+++ lou.js
@@ -1,64 +1,26 @@
-//using promises to handle asynchronous events and external data
-
-//ready function
-//document.addEventListener("DOMContentLoaded", function() {
-    //define an array to hold the slideshow data 
-    document.addEventListener("DOMContentLoaded", function()
-    {
- let slideshow = [];
-    //find datafile 
-  const slides=document.querySelector("#slides");
-  const datafile = "me.json";
-  const request = new Request(datafile);
-  //call the fetch method and store the response to a variable
-  let response = fetch(datafile);
-  //fetch returns a Promise which you can then handle with then() and catch(). When the request completes, a response object is returned
-  fetch(request)
-  .then(response => response.json())
-  .then(data => {
-   for (const me of data.me) {
-        //create new li to hold slide content
- let slide = document.createElement('li');
-    slide.innerHTML = `<li class="slide"><span class='title'>${me.title}</span><img src="${me.image}"><span class="caption">${me.caption}</span></div><audio src="${me.audio}"</audio></li>`
- //append to the ul
- slides.appendChild(slide);
-    }
-
-    })
-
-.catch(console.error);
-
-});
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-console.log(slideIndex)
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+window.onload = init;
+	
+function init() {
+	myVidsIndex = 0;
+	let button = document.getElementById('button');
+	console.log(button)
+	button.addEventListener("click", nextVid, 1);
 }
 
-function showSlides (n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1){slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display="none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display ="block";
-    dots[slideIndex-1].className += " active";
-}
+let myVidsArray = ["vid1", "vid2",];
+let myVidsIndex = 0;
 
-window.addEventListener("load",function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, 4000);
-    audio.play();
-})
+function nextVid(direction) {
+	console.log("clicked");
+	//increment / decrement array
+	myVidsIndex = myVidsIndex + 1;	
+	if (myVidsIndex == myVidsArray.length){
+		myVidsIndex = 0
+	}
+	// update 
+	element = document.getElementById('vid');
+	console.log("Your element is: ");
+	console.log(element);
+	console.log(myVidsIndex, myVidsArray[myVidsIndex]);
+	element.src = "images/" + myVidsArray[myVidsIndex] + ".MOV";
+}
