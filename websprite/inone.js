df274a32adab1e631829eb905750f274a3c7c6e2
salal812 <salal812@newschool.edu>
2022-05-01 15:57:15 -0400

    added buttons

--- websprite/inone.js
+++ websprite/inone.js
@@ -8,7 +8,16 @@ function init() {
 
     let prev = document.getElementById('prev');
     console.log(prev)
-    prev.addEventListener("click", prevPic, -1)
+    prev.addEventListener("click", prevPic, -1);
+
+	myPhotosIndex = 0;
+	let more = document.getElementById('more');
+	console.log(more)
+	more.addEventListener("click", morePhoto, 1);
+
+    let less = document.getElementById('less');
+    console.log(less)
+    less.addEventListener("click", lessPhoto, -1);
 
 }
 
@@ -27,7 +36,7 @@ function nextPic(direction) {
 	console.log("Your element is: ");
 	console.log(element);
 	console.log(myPicsIndex, myPicsArray[myPicsIndex]);
-	element.src = "/websprite/" +"images/" + myPicsArray[myPicsIndex] + ".jpg";
+	element.src = "./images/" + myPicsArray[myPicsIndex] + ".jpg";
 }
 
 function prevPic(direction) {
@@ -35,12 +44,46 @@ function prevPic(direction) {
 	//increment / decrement array
 	myPicsIndex = myPicsIndex - 1;	
 	if (myPicsIndex == -1){
-		myPicsIndex = myPicsArray.length - 1
+		myPicsIndex = myPhotosArray.length - 1
 	}
 	// update 
-	element = document.getElementById("pic");
+	element = document.getElementById('pic');
 	console.log("Your element is: ");
 	console.log(element);
 	console.log(myPicsIndex, myPicsArray[myPicsIndex]);
-	element.src = "/websprite/" +"images/" + myPicsArray[myPicsIndex] + ".jpg";
+	element.src = "./images/" + myPicsArray[myPicsIndex] + ".jpg";
+}
+
+let myPhotosArray = ["loom1", "loom2", "loom3", "loom4", "loom5", "loom6", "loom7", "loom9", "loom10", "loom11", "loom12", "loom13",];
+let myPhotosIndex = 0;
+
+
+function lessPhoto(direction) {
+	console.log("clicked");
+	//increment / decrement array
+	myPhotosIndex = myPhotosIndex - 1;	
+	if (myPhotosIndex == -1){
+		myPhotosIndex = myPhotosArray.length - 1
+	}
+	// update 
+	element = document.getElementById("photo");
+	console.log("Your element is: ");
+	console.log(element);
+	console.log(myPhotosIndex, myPhotosArray[myPhotosIndex]);
+	element.src = "./images/" + myPhotosArray[myPhotosIndex] + ".png";
+}
+
+function morePhoto(direction) {
+	console.log("clicked");
+	//increment / decrement array
+	myPhotosIndex = myPhotosIndex + 1;	
+	if (myPhotosIndex == myPhotosArray.length){
+		myPhotosIndex = 0
+	}
+	// update 
+	element = document.getElementById("photo");
+	console.log("Your element is: ");
+	console.log(element);
+	console.log(myPhotosIndex, myPhotosArray[myPhotosIndex]);
+	element.src = "./images/" + myPhotosArray[myPhotosIndex] + ".png";
 }
